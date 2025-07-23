e684b03e39ed8dd2251e965dbf207851cf7e5e5b
Rafael Gimenez <rafaelgimenez@192.168.1.15>
2025-07-23 12:56:54 -0300

    Habilitar CORS para Vercel

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from routers import tareas
 class CustomJSONResponse(JSONResponse):
     media_type = "application/json; charset=utf-8"
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "https://tareas-frontend-kwhq.vercel.app",  # Dominio del frontend en Vercel
+]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Solo permite este origen
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app = FastAPI(default_response_class=CustomJSONResponse)
 
 app.include_router(tareas.router)
