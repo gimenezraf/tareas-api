fcf4fdf2ee6e2aca9ab94fbd33b0e7e58105753d
Rafael Gimenez <rafaelgimenez@192.168.1.15>
2025-07-23 13:03:27 -0300

    Corrijo rutas sin barra final para evitar redirección y error CORS

--- routers/tareas.py
+++ routers/tareas.py
@@ -16,10 +16,10 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/tareas/", response_model=schemas.Tarea)
+@router.post("/tareas", response_model=schemas.Tarea)
 def crear_tarea(tarea: schemas.TareaCreate, db: Session = Depends(get_db)):
     return crud.crear_tarea(db=db, tarea=tarea)
 
-@router.get("/tareas/", response_model=List[schemas.Tarea])
+@router.get("/tareas", response_model=List[schemas.Tarea])
 def listar_tareas(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.obtener_tareas(db=db, skip=skip, limit=limit)
