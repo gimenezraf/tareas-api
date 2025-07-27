7e125f2dcaf50bff616adc4ae7bb65b32802192b
Rafael Gimenez <rafaelgimenez@192.168.1.15>
2025-07-27 18:45:07 -0300

    Habilitar CORS para Vercel

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import tareas
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://tareas-frontend-kwhq.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(tareas.router)
 
